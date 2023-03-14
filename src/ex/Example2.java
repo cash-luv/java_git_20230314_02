4a75ef9672d05123755555ba20b1f2d2bb287167
cash-luv <mino210125@naver.com>
2023-03-14 10:16:30 +0900

    numberTable 실습

---
+++ src/ex/Example2.java
@@ -0,0 +1,12 @@
+package ex;
+
+public class Example2 {
+	public static void numberTable() {
+        for(int i=2; i<=9; i++) {
+            for(int j=1; j<=9; j++) {
+                System.out.println(i + " x " + j + " = " + i*j);
+            }
+            System.out.println();
+        }
+    }
+}
